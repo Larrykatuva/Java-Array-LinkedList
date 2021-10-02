1fe71145f579527852cb912c1be50ad8e6513298
Larrykatuva <larry.katuva@gmail.com>
2021-10-02 23:53:14 +0300

    All Solution

---
+++ src/LinkedListApp.java
@@ -0,0 +1,15 @@
+
+public class LinkedListApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 LinkedList theList = new LinkedList(); // make new list
+		 theList.insertFirst(22, 29); // insert four items
+		 theList.insertFirst(44, 49);
+		 theList.insertFirst(66, 69);
+		 theList.insertFirst(88, 89);
+		 theList.displayList(); 
+
+	}
+
+}
